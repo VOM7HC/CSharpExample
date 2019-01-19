35b4756ef370d8af0a932f3fed356cb3eef4919e
Khoi Vo Minh <khoiminhvo1996@icloud.com>
2019-01-19 13:11:31 +0700

    - Update SDK to 7.1.1 - Commit variables sample

---
+++ WP7/1_9_Variables/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace _1_9_Variables
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        private int var_a = 10;
+        private int var_b = 10;
+        private int var_c = 10;
+
+        private string result = "0";
+        private string current_answer_1;
+        private string current_answer_2;
+        private string current_question;
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            current_answer_1 = txb_answer_1.Text;
+            current_answer_2 = txb_answer_2.Text;
+            current_question = txb_question_math_2.Text;
+
+            txb_question_math_1.Text += (var_a.ToString() + " + " + var_b.ToString() + "?");
+            txb_question_math_2.Text += (" Question 1 (currently: " + result + ") + " + var_c.ToString() + "?");
+        }
+
+        private void btn_touch_1_Click(object sender, RoutedEventArgs e)
+        {
+            result = (var_a + var_b).ToString();
+            txb_answer_1.Text = current_answer_1 + result;
+            txb_question_math_2.Text = current_question + (" Question 1 (currently: " + result + ") + " + var_c.ToString() + "?");
+        }
+
+        private void btn_touch_2_Click(object sender, RoutedEventArgs e)
+        {
+            txb_answer_2.Text = current_answer_2 + (int.Parse(result) + var_c).ToString();
+        }
+    }
+}
---
+++ WP7/1_9_Variables/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("_1_9_Variables")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("_1_9_Variables")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cdaa940b-0560-45e5-9021-d946c8206e94")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
