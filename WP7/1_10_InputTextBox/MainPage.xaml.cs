f169ea06944887b8fa2222d855952eea21c36a93
Khoi Vo Minh <khoiminhvo1996@icloud.com>
2019-01-20 09:11:07 +0700

    Add Input to text box and focus on launch app

---
+++ WP7/1_10_InputTextBox/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace _1_10_InputTextBox
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void txt_input_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txb_show.Text = txt_input.Text;
+        }
+
+        private void btn_clear_Click(object sender, RoutedEventArgs e)
+        {
+            txb_show.Text = "Show here!!";
+            txt_input.Text = "";
+
+            txt_input.Focus();
+        }
+
+        private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            txt_input.Focus();
+        }
+    }
+}
