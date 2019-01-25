f0db46dd5e4cf15c5472e813c77c6abb2160fba1
Khoi Vo Minh <khoiminhvo1996@icloud.com>
2019-01-26 00:05:45 +0700

    Add For Iteration example

---
+++ WP7/1_14_ForIterations/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace _1_14_ForIterations
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        private string startStr = "Show number: ";
+        private int num;
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();            
+            txt_show.Text = startStr;
+        }
+
+        private void txb_input_Tap(object sender, GestureEventArgs e)
+        {
+            txb_input.Text = "";
+        }
+
+        private void btn_touch_Click(object sender, RoutedEventArgs e)
+        {
+            string resStr = "";
+            for (int i = num; i < num + 3; i++)
+            {
+                resStr += startStr + i.ToString() + System.Environment.NewLine;
+            }
+            txt_show.Text = resStr;
+        }
+
+        private void txb_input_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txb_input.Text == "" || txb_input.Text == "Input number here")
+                num = 0;
+            else
+                num = int.Parse(txb_input.Text);
+        }
+    }
+}
---
+++ WP7/1_14_ForIterations/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("_1_14_ForIterations")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("_1_14_ForIterations")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a0907528-5277-49bd-b18e-1046a17cc957")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
